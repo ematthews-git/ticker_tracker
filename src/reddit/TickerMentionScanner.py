da9d222910ba96bfacfc584877cac2792f0ae6ff
idektbhuah <matthewsethan43@gmail.com>
2025-11-11 16:25:59 +1300

    Updated documentation

--- src/reddit/TickerMentionScanner.py
+++ src/reddit/TickerMentionScanner.py
@@ -1,14 +1,26 @@
 import re
 
 class TickerMentionScanner:
-    def __init__(self, valid_tickers):
-        self.valid = set(valid_tickers)
+    """Scans for Ticker Mentions
+
+    """
+    def __init__(self, invalid_tickers):
+        self.invalid = set(invalid_tickers)
     
-    def CountMentions(self, text_list):
+    #Counts mentions of valid tickers into dictionary from a list of text
+    def count_mentions(self, text_list):
+        """finds the mention of any valid ticker in the list of text parsed
+
+        Args:
+            text_list (Iterable[str]): The list of text items to search
+
+        Returns:
+            dict[str, int]: A dictionary mapping each ticker symbol to the number of times it was mentioned
+        """
         counts = {}
         for text in text_list:
             words = re.findall(r'\b[A-Z]{2,5}\b', text) #capital letters + 2 to 5 characters
             for w in words:
-                if w in self.valid:
+                if w not in self.invalid:
                     counts[w] = counts.get(w, 0) + 1 #existing val += 1
         return counts
--- src/storage/Database.py
+++ src/storage/Database.py
@@ -2,11 +2,22 @@ import sqlite3
 from datetime import datetime
 
 class Database:
+    """The database holding each ticker, count value pair
+
+    SQLite database which holds entries per timestamp. 
+    EX: "TSLA", "2025-11-11 12:00:00", "300"
+        "TSLA", "2025-11-11 13:00:00", "200"
+    """
     def __init__(self, db_path="data.db"):
         self.connection = sqlite3.connect(db_path)
         self.connection.execute("CREATE TABLE IF NOT EXISTS mentions (ticker TEXT, timestamp TEXT, mentions INTEGER)")
     
-    def InsertMentionCounts(self, counts):
+    def insert_mention_counts(self, counts):
+        """Adds each ticker in counts to the database with a timestamp
+
+        Args:
+            counts (dict[str, int]): A dictionary holding a ticker and count value pair
+        """
         timestamp = datetime.utcnow().isoformat()
         for ticker, value in counts.items():
             self.connection.execute(
