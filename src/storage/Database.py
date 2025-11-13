3cb0ef269ea9e30fbf0aa938918da4bfc4da40ea
idektbhuah <matthewsethan43@gmail.com>
2025-11-13 17:19:54 +1300

    ready 2 run

--- src/storage/Database.py
+++ src/storage/Database.py
@@ -6,18 +6,18 @@ def insert_mention_counts(DB_PATH, counts):
 
     Args:
         DB_PATH (str): The database path
-        counts (dict[str, int]): A dictionary holding a ticker and count value pair
+        counts (dict[tuple[str, str], int]): A dictionary mapping (ticker, subreddit) tuples to mention counts
     """
     timestamp = datetime.utcnow().isoformat()
 
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
-    for ticker, count in counts:
+    for (ticker, subreddit), count in counts.items():
         cursor.execute("""
-            INSERT OR IGNORE INTO mentions (ticker, timestamp, mention_count)
-            VALUES (?, ?, ?);
-        """, (ticker.upper(), timestamp, count))
+            INSERT OR IGNORE INTO mentions (ticker, subreddit, timestamp, mention_count)
+            VALUES (?, ?, ?, ?);
+        """, (ticker.upper(), subreddit, timestamp, count))
     
     conn.commit()
     conn.close()
