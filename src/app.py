f312f038c083ca1ba0e17471fca7b4289b3b3cfa
idektbhuah <matthewsethan43@gmail.com>
2025-11-13 16:32:20 +1300

    initial scheduler implementation

--- src/app.py
+++ src/app.py
@@ -1,16 +1,59 @@
 from reddit.RedditClient import RedditClient
 from reddit.TickerMentionScanner import TickerMentionScanner
-from storage.Database import Database
-from config import CLIENT_ID, CLIENT_SECRET, USER_AGENT
+from config import CLIENT_ID, CLIENT_SECRET, USER_AGENT, INVALID, DB_PATH
+import schedule
+from datetime import datetime
+import time
+from storage.Database import insert_mention_counts
 
-INVALID = ['YOLO', 'HOLD', 'HODL']
+def collect_data():
+    """Collects ticker mentions and saves to database
+    """
+    print(f"[{datetime.now}] starting data collection...")
+    try:
+        #initialise clients
+        reddit = RedditClient(CLIENT_ID, CLIENT_SECRET, USER_AGENT)
+        scanner = TickerMentionScanner(INVALID)
 
-reddit = RedditClient(CLIENT_ID, CLIENT_SECRET, USER_AGENT)
-scanner = TickerMentionScanner(INVALID)
-#db = Database()
+        #fetch posts
+        print("Fetching posts from r/pennystocks...")
+        posts = list(reddit.fetch_recent_posts("pennystocks", limit=500))
+        print(f"Fetched {len(posts)} posts/comments")
 
-texts = list(reddit.fetch_recent_posts("wallstreetbets", limit=300))
-counts = scanner.count_mentions(texts)
-#db.insert_mention_counts(counts)
+        #count mentions and save posts
+        print("Counting ticker mentions...")
+        counts = scanner.process_mentions(posts)
+        print(f"Completed. unique tickers: {len(counts)}")
 
-print("done. Counts: ", counts)
+        #save mentions
+        if counts:
+            insert_mention_counts(DB_PATH, counts)
+            print(f"Saved items to mentions database")
+        else:
+            print("No ticker mentions found")
+
+    except Exception as e:
+        print(f"[ERROR] Data collection failed: {e}")
+
+def main():
+    """Runs the scheduler every hour at :00
+    """
+    print("=" * 50)
+    print("Reddit Ticker Mention Collector")
+    print("=" * 50)
+    print(f"Started at: {datetime.now}")
+    print("Schedule: Every hour")
+    print("Press Cmnd+C to stop\n")
+
+    #schedule the job
+    schedule.every().hour.at(":00").do(collect_data)
+    
+    try:
+        while True:
+            schedule.run_pending()
+            time.sleep(60)
+    except KeyboardInterrupt:
+        print("\n\n Scheduler stopped by user")
+
+if __name__ == "__main__":
+    main()
--- src/config.py
+++ src/config.py
@@ -3,6 +3,9 @@ import os
 
 load_dotenv()
 
+INVALID = ['YOLO', 'HOLD', 'HODL', 'WSB', 'CEO', 'CFO', 'IPO', 'ETF']
+SUBREDDITS = ["pennystocks", "10xpennystocks", "Daytrading", "SmallStreetBets"]
+
 CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
 CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
 USER_AGENT = os.getenv("REDDIT_USER_AGENT")
