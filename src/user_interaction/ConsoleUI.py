710fbc9036ab3132640829fabcaf7d869d73f3e1
idektbhuah <matthewsethan43@gmail.com>
2025-11-14 21:17:10 +1300

    working

--- src/user_interaction/ConsoleUI.py
+++ src/user_interaction/ConsoleUI.py
@@ -8,7 +8,7 @@ from pathlib import Path
 # Add parent directory to path to import config and models
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
-from config import DB_PATH
+from config import DB_PATH, SUBREDDITS
 from models import MentionDataPoint
 from storage import Database
 
@@ -33,26 +33,46 @@ def main():
             
             ticker = input("Enter desired ticker(e.g 'ABC'): ")
             graph_ticker(ticker)
+            print("Graphing complete")
+            entry = ' '
+            continue
 
 
 def graph_ticker(ticker: str):
-    #get times
     try:
+        #get times
         start_time_input = int(input("Enter the number of days you want to view(Max 3): "))
         if start_time_input > 3: start_time_input = 3
         now = datetime.now()
         dif = timedelta(days=start_time_input)
         start_time = now - dif
 
+        ####
         plot_points = list(Database.fetch_ticker_mentions(DB_PATH, ticker, start_time, now))
 
-        x_axis = []
-        y_axis = []
-        for point in plot_points:
-            x_axis.append(point.timestamp)
-            y_axis.append(point.mention_count)
-    
-        plt.plot(x_axis, y_axis)
+        #these are lists containg each axis list for the subreddits
+        all_x_axis = []
+        all_y_axis = []
+
+        for sub in SUBREDDITS:
+            x = []
+            y = []
+            for point in plot_points:
+                if point.subreddit == sub:
+                    x.append(point.timestamp)
+                    y.append(point.mention_count)
+            
+            all_x_axis.append(x)
+            all_y_axis.append(y)
+        
+        for i, sub in enumerate(SUBREDDITS):
+            plt.plot(all_x_axis[i], all_y_axis[i], label=sub)
+
+        plt.title(f"""{ticker.upper()} mentions from 
+                  {start_time.replace(microsecond=0, second=0, minute=0)} to {now.replace(microsecond=0, second=0, minute=0)}""")
+        plt.legend()
+        plt.grid(True)
+        plt.tight_layout()
         plt.show()
 
     except Exception as e:
@@ -61,5 +81,3 @@ def graph_ticker(ticker: str):
 if __name__ == "__main__":
     main()
 
-
-    
