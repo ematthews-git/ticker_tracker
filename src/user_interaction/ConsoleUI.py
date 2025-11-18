26007756ca3456d19ddcaacc094537fe5553ad2c
idektbhuah <matthewsethan43@gmail.com>
2025-11-18 13:48:33 +1300

    fixing timezones. commit also mixed with fetching database info

--- src/user_interaction/ConsoleUI.py
+++ src/user_interaction/ConsoleUI.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
 import numpy as np
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 import sys
 from pathlib import Path
@@ -20,7 +20,7 @@ def main():
     entry = " "
     while entry != "9":
         print("1: Enter ticker to graph it")
-        print("2: List tickers by highest Z score")
+        print("2: List most popular tickers")
         print("3: Find new tickers")
         print("9: Exit")
 
@@ -36,16 +36,25 @@ def main():
             print("Graphing complete")
             entry = ' '
             continue
+        elif entry == "2":
+            print("*" * 50)
+            print("2: List most popular tickers")
+            print("*" * 50)
+            display_popular_tickers()
+            entry = ''
+            continue
 
+            
 
 def graph_ticker(ticker: str):
     try:
         #get times
         start_time_input = int(input("Enter the number of days you want to view(Max 3): "))
         if start_time_input > 3: start_time_input = 3
-        now = datetime.now()
+        now = datetime.now(timezone.utc)
         dif = timedelta(days=start_time_input)
         start_time = now - dif
+        print(f"{start_time}, \n {now}")
 
         ####
         plot_points = list(Database.fetch_ticker_mentions(DB_URL, ticker, start_time, now))
@@ -77,6 +86,41 @@ def graph_ticker(ticker: str):
 
     except Exception as e:
         print(f"[ERROR] error with graphing ticker: {e}")
+
+def display_popular_tickers():
+    """Asks the user for parameters and displays the most popular tickers based on those parameters.
+    """
+    try: 
+        timeframe = input("Enter timeframe(e.g '12h', '1d', '1w'): ")
+        now = datetime.now(timezone.utc)
+        if timeframe[-1] == "h":
+            dif = timedelta(hours=int(timeframe[:-1]))
+        elif timeframe[-1] == "d":
+            dif = timedelta(days=int(timeframe[:-1]))
+        elif timeframe[-1] == "w":
+            dif = timedelta(weeks=int(timeframe[:-1]))
+        else:
+            print("Timeframe formatted incorrectly")
+            return
+    except:
+        print("Timeframe formatted incorrectly")
+
+    start_time = now - dif
+    print(f"{start_time}, \n {now}")
+
+    amount = int(input("How many tickers would you like to see(min = 10, max = 30): "))
+    if amount > 30: amount = 30
+    elif amount < 10: amount = 10
+
+    popular_tickers = Database.fetch_popular_tickers(DB_URL, start_time, now, amount)
+    print(len(popular_tickers))
+
+    #displaying
+    for ticker, points in popular_tickers.items():
+        total_mentions = sum(dp.mention_count for dp in points)
+        print(f"{ticker}: {total_mentions}")
+
+
 
 if __name__ == "__main__":
     main()
