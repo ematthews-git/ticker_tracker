ef9efa976af67474643bf9370ea29b44e9155581
idektbhuah <matthewsethan43@gmail.com>
2026-01-15 11:53:06 +1300

    inital price fetching skeleton

--- src/market/price_fetcher.py
+++ src/market/price_fetcher.py
@@ -1,4 +1,6 @@
 import yfinance as yf
+from datetime import datetime, timedelta, timezone
+from utils import helper
 
 from logging_setup import configure_logging
 import logging
@@ -12,4 +14,25 @@ class PriceCollector:
     """Collects and stores stock prices for relevant tickers"""
     def __init__(self, connection_pool):
          self.connection_pool = connection_pool
+    
+    def collect_and_store_prices(self, current_time: datetime = None) -> None:
          
+        if current_time is None:
+            current_time = datetime.now(timezone.utc).replace(minute=0, second=0, microsecond=0)
+
+        logger.info(f"Collecting prices for {current_time}")
+
+        #get all active tickers from the last 24 hours
+        from datetime import timedelta
+        start_time = current_time - timedelta(hours=24)
+        
+        active_tickers = helper.get_active_tickers(
+            self.connection_pool,
+            start_time, 
+            current_time,
+            min_mentions=2  
+        )
+
+        logger.info(f"Found {len(active_tickers)} active tickers")
+
+
